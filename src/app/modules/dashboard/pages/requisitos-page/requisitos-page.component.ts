15c4c8d566bd62b89a4fc4365c62ea666d3e2cfa
Anyoza Ccenhua Jean Kevin <janyoza@sunat.gob.pe>
2024-05-09 15:15:02 -0500

    shared requisitos

--- src/app/modules/dashboard/pages/requisitos-page/requisitos-page.component.ts
+++ src/app/modules/dashboard/pages/requisitos-page/requisitos-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Tupa05TabContainerComponent } from 'src/app/modules/tupa-05/components/tupa-05-tab-container/tupa-05-tab-container.component';
 import { DashboardHeaderComponent } from '../../components/dashboard-header/dashboard-header.component';
-import { RequisitosTabContainerComponent } from 'src/app/modules/requisitos/components/requisitos-tab-container/requisitos-tab-container.component';
+import { RequisitosTabContainerComponent } from 'src/app/modules/shared/requisitos/components/requisitos-tab-container/requisitos-tab-container.component';
 
 @Component({
   selector: 'app-requisitos-page',
