11c310d152756875407c9d9d2275e69eaee8223c
Anyoza Ccenhua Jean Kevin <janyoza@sunat.gob.pe>
2024-05-09 14:01:10 -0500

    table requisitos

--- src/app/modules/requisitos/components/requisitos-adjuntos/requisitos-adjuntos.component.ts
+++ src/app/modules/requisitos/components/requisitos-adjuntos/requisitos-adjuntos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RequisitosTableComponent } from '../requisitos-table/requisitos-table.component';
 
 @Component({
   selector: 'app-requisitos-adjuntos',
   standalone: true,
-  imports: [],
+  imports: [RequisitosTableComponent],
   templateUrl: './requisitos-adjuntos.component.html',
 })
 export class RequisitosAdjuntosComponent {
