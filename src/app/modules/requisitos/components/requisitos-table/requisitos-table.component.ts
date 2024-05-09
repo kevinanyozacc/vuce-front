fc1da5045adb4e2ea4ece70999a989fe10406845
Anyoza Ccenhua Jean Kevin <janyoza@sunat.gob.pe>
2024-05-09 14:06:23 -0500

    table requisitos up

---
+++ src/app/modules/requisitos/components/requisitos-table/requisitos-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-requisitos-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './requisitos-table.component.html',
+})
+export class RequisitosTableComponent {
+
+}
